1820528548de945062e356e8a9c6a4f2dc9679a8
D.Patel <d.patel@interproductec.com>
2021-03-27 22:36:08 +0100

    remove  sig term handler

--- client/utils.py
+++ client/utils.py
@@ -8,7 +8,6 @@ class ShutdownHandler(object):
     def __init__(self):
 
         signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
         self.shutdown = True
